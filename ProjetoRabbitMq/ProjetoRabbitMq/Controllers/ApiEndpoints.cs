a12de9e49bc949c1e0c28a47907f479ceab788ef
luispfatdo <luis.paulo.tdo@gmail.com>
2026-01-20 03:55:21 -0300

    Criando endpoint para abrir uma solicitação de relatório.

--- ProjetoRabbitMq/ProjetoRabbitMq/Controllers/ApiEndpoints.cs
+++ ProjetoRabbitMq/ProjetoRabbitMq/Controllers/ApiEndpoints.cs
@@ -1,9 +1,22 @@
-﻿namespace ProjetoRabbitMq.Controllers;
+﻿using ProjetoRabbitMq.Reports;
+
+namespace ProjetoRabbitMq.Controllers;
 
 public static class ApiEndpoints
 {
     public static void AddApiEndpoints(this WebApplication app)
     {
-        app.MapGet("hello-world", () => new { saudacao = "Hello World!" });
+        app.MapPost("solicitar-relatorio/{name}", (string name) =>
+        {
+            var solicitation = new ReportSolicitation()
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Status = "Pendente",
+                ProcessedTime = null
+            };
+
+            ReportList.Reports.Add(solicitation);
+        });
     }
 }
