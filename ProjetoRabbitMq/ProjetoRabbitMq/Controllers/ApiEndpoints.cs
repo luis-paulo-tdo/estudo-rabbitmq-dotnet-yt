8528d24e5057e8ed214f42baa791c1562623f771
luispfatdo <luis.paulo.tdo@gmail.com>
2026-01-20 03:46:32 -0300

    Criando um primeiro endpoint para teste inicial do projeto.

---
+++ ProjetoRabbitMq/ProjetoRabbitMq/Controllers/ApiEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoRabbitMq.Controllers;
+
+public static class ApiEndpoints
+{
+    public static void AddApiEndpoints(this WebApplication app)
+    {
+        app.MapGet("hello-world", () => new { saudacao = "Hello World!" });
+    }
+}
--- ProjetoRabbitMq/ProjetoRabbitMq/Program.cs
+++ ProjetoRabbitMq/ProjetoRabbitMq/Program.cs
@@ -1,3 +1,5 @@
+using ProjetoRabbitMq.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.AddApiEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
