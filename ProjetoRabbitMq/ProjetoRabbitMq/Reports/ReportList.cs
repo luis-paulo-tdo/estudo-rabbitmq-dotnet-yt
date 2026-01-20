c34a86ac39aea106168272be409253a7fb3c9892
luispfatdo <luis.paulo.tdo@gmail.com>
2026-01-20 03:55:05 -0300

    Modelando uma classe para armazenar em memória uma lista de relatórios.

---
+++ ProjetoRabbitMq/ProjetoRabbitMq/Reports/ReportList.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoRabbitMq.Reports;
+
+public static class ReportList
+{
+    public static List<ReportSolicitation> Reports = new();
+}
+
+public class ReportSolicitation
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Status { get; set; }
+    public DateTime? ProcessedTime { get; set; }
+}
