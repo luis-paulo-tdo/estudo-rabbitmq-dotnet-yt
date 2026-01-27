2c6d92d96f2e374157a9efc7ea59ed7f978d8a57
luispfatdo <luis.paulo.tdo@gmail.com>
2026-01-27 03:40:50 -0300

    Instalação do MassTransit para fazer o RabbitMq funcionar.

---
+++ ProjetoRabbitMq/ProjetoRabbitMq/Extensions/AppExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoRabbitMq.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void AddRabbitMqService(this IServiceCollection services)
+        {
+
+        }
+    }
+}
